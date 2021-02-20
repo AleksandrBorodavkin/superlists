688d77df97b19452eb55fcfdc3e93859be7ad059
Alexandr Borodavkin <init220@gmail.com>
2021-02-21 06:31:53 +0700

    simplify home view, no longer displays all items.
    --ch07l018--

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,7 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
-    return render(request, 'lists/home.html', {'items': items})
+    return render(request, 'lists/home.html')
 
 
 def view_list(request):
