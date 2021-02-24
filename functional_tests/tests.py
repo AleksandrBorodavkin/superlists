81d2b04f4ada463a6a7df288a437cecfaa5013f9
Aleksandr Borodavkin <init220@gmail.com>
2021-02-24 18:31:46 +0700

    extend FT to check a second step --ch08l003--

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -125,3 +125,14 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=10
         )
+        # Она начинает новый список и видит, что поле ввода там тоже
+        # аккуратно центировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
