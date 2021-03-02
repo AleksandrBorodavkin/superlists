11aecfb91e139d6e7bb488a33b0f24d736e568f9
Aleksandr Borodavkin <init220@gmail.com>
2021-03-02 09:12:58 +0700

    fix

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
-REPO_URL = 'https://github.com/hjwp/book-example.git'
+REPO_URL = 'git@github.com:init220/superlists.git'
 
 
 def deploy():
